d5446201e60080279a1a43fa4ff683c885b4753c
Anam Kazi <anam@iii.global>
2017-10-17 14:47:54 -0700

    first push

---
+++ src/components/views/anam/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="column items-center justify-between fill">
+        <q-btn class="bg-orange-5 text-white">{{ myName }}</q-btn>
+        <q-btn class="bg-orange-5 text-white">Shahan Kazi</q-btn>
+        <q-btn class="bg-orange-5 text-white">Fahad Kazi</q-btn>
+    </div>
+</template>
+
+<script>
+import script from './script'
+
+export default script;
+</script>
+
+<style scoped>
+.fill  {
+    width: 100%;
+    height: 100vh;
+}
+</style>
